02aaee7fa2e683ed390032ff918d3a87bbcf2402
chuksholymoses <chuksyb27@gmail.com>
2023-07-24 07:20:17 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_strlen - Write a function that returns the length of a string
+ @s: strings to be checked
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+int string_length = 0;
+while (s[string_length])
+string_length++;
+return (string_length);
+}
